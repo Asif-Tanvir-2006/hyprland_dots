ec4913c95dd36cddf0f24f663ecfb42aa29847d3
Asif <asiftanvir2006@gmail.com>
2023-09-23 19:57:12 +0530

    fixed wifi.py

--- eww_1/Scripts/wifi.py
+++ eww_1/Scripts/wifi.py
@@ -3,11 +3,13 @@
 import subprocess
 
 icons = ["󰤨  Online", "󰤭  Offline"]
-hostname = "gnu.org"  # Change this to "gnu.org" or any other host you want to ping
 
-# Check network connectivity using ping
+# Check network connectivity using nmcli
 try:
-    subprocess.check_output(["ping", "-c", "1", hostname])
-    print(icons[0])  # Network is reachable
+    check = subprocess.check_output(["nmcli", "networking", "connectivity", "check"]).decode("utf-8")
+    if 'none' in check:
+        print(icons[1])  # Network is reachable
+    if 'full' in check: 
+        print(icons[0])  # Netwrok is not reachable
 except subprocess.CalledProcessError:
     print(icons[1])  # Network is unreachable
--- eww_2/Scripts/wifi.py
+++ eww_2/Scripts/wifi.py
@@ -6,7 +6,10 @@ icons = ["󰤨  Online", "󰤭  Offline"]
 
 # Check network connectivity using nmcli
 try:
-    subprocess.check_output(["nmcli", "networking", "connectivity", "check"])
-    print(icons[0])  # Network is reachable
+    check = subprocess.check_output(["nmcli", "networking", "connectivity", "check"]).decode("utf-8")
+    if 'none' in check:
+        print(icons[1])  # Network is reachable
+    if 'full' in check: 
+        print(icons[0])  # Netwrok is not reachable
 except subprocess.CalledProcessError:
     print(icons[1])  # Network is unreachable
